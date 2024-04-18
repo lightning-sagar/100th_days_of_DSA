05c7bedbceacc797930fef910b19d65c737fc7ad
231B275 Sagar <lightningsagar0@gmail.com>
2024-04-18 22:12:27 +0530

    day17

---
+++ STACK/day17/reverse_2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;//0-1-4-3-2-5
+        
+        // moce till it come to left and store it in prev;
+        for (int i = 1; i < left; ++i) {
+            prev = prev->next;
+        }
+        
+        // Reverse it until it tends to right
+        ListNode* curr = prev->next;
+        for (int i = left; i < right; ++i) {
+            ListNode* next = curr->next;
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+        }
+        
+        return dummy->next;
+    }
+};
